842cd802861b1ad6ce7f23ea1bb8393502135034
Rick Sanchez <rickcomics@outlook.com>
2026-04-30 12:25:12 +0300

    fix: simplify config for successful build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  // Убрали skipWaiting и register, плагин сам разберется по дефолту
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Оставляем пустым, чтобы не было конфликтов типов с экспериментальными фичами
+  /* тут пусто */
 };
-
-export default withPWA(nextConfig);
+export default nextConfig;
